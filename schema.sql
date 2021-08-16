421970de6122784e2adb1c1a639533b186867200
rattrapp <zell0077@umn.edu>
2021-08-16 16:46:52 -0500

    7.2.4

--- schema.sql
+++ schema.sql
@@ -7,6 +7,7 @@ CREATE TABLE departments (
      UNIQUE (dept_name)
 );
 
+
 CREATE TABLE employees (
      emp_no INT NOT NULL,
      birth_date DATE NOT NULL,
@@ -21,11 +22,9 @@ CREATE TABLE dept_manager (
 dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+    to_date DATE NOT NULL
 );
+
 
 CREATE TABLE salaries (
   emp_no INT NOT NULL,
@@ -36,5 +35,19 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
-SELECT * FROM departments;
+CREATE TABLE titles (
+  emp_no int NOT NULL,
+  title varchar NOT NULL,
+  to_date date NOT NULL,
+  from_date date NOT NULL
+	);
+
+	
+CREATE TABLE dept_emp (
+emp_no int NOT NULL, 
+dept_no VARCHAR(4) NOT NULL,
+from_date date NOT NULL,
+to_date date NOT NULL	
+)
+
 
